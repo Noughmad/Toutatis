1e11ed35a541a46ce83aeeb3fa485899a4656fb7
Miha Čančula <miha@noughmad.eu>
2013-01-29 14:08:07 +0100

    Add the basic interface methods

--- toutatis.h
+++ toutatis.h
@@ -1,16 +1,36 @@
 #ifndef Toutatis_H
 #define Toutatis_H
 
-#include <QtCore/QObject>
+#include <QObject>
+#include <QStringList>
 
 class Toutatis : public QObject
 {
-Q_OBJECT
+    Q_OBJECT
+
 public:
     Toutatis(QObject* parent = 0);
     virtual ~Toutatis();
 
-private slots:
+
+public slots:
+    QStringList projects() const;
+    void createProject(const QString& name, const QString& client = QString());
+
+    QStringList tasks(const QString& project);
+    void createTask(const QString& project, const QString& task);
+    void removeTask(const QString& project, const QString& task);
+    void renameTask(const QString& project, const QString& task, const QString& name);
+
+    void currentProjectAndTask(QString& project, QString& task);
+    void startTask(const QString& project, const QString& task);
+    void stopTask(const QString& project, const QString& task);
+    void stop();
+
+    void addNote(const QString& project, const QString& task, const QString& note);
+    void addEvent(const QString& project, const QString& task, const QString& eventType, const QString& message);
+
+private:
     void output();
 };
 
